3e66818341c313583e5e9961dd3cf0606eebf2f6
Jossyjoe1 <126296142+Jossyjoe1@users.noreply.github.com>
2026-02-16 11:59:09 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PickOnTheGoMVP from "@/app/PickOnTheGoMVP";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PickOnTheGoMVP />
+  </React.StrictMode>
+);
